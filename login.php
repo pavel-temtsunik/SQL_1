cc39e8fd9e8beab5822aea5c01fcf0df14e18579
pavel-temtsunik <pavel.hvoya@gmail.com>
2015-07-23 00:22:20 +0300

    credentials

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php // login.php
+ $db_hostname = 'localhost';
+ $db_database = 'publications';
+ $db_username = 'root';
+ $db_password = '';
+?>/**
+ * Created by PhpStorm.
+ * User: Smart
+ * Date: 7/23/2015
+ * Time: 12:19 AM
+ */
